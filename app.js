1f686a326982cbfcfc073ef48cb7bea089f70633
Kevin <mybaseball52@gmail.com>
2017-07-22 22:00:28 +0800

    bundling

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 var express = require('express');
-
 var app = express();
-
 var port = 5000;
- 
+
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
-
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.send('Hello World');
 });
 
-app.get('/books', function(req, res){
+app.get('/books', function(req, res) {
     res.send('Hello Books');
 });
 
-app.listen(port, function(err){
+app.listen(port, function(err) {
     console.log('running server on port ' + port);
 });
